4e5eef8790bfd736fb73cce08fa3db60552c3b03
Alex <amonsalveru@gmail.com>
2024-01-15 23:13:08 -0300

    Stack Navigator

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Slot, Stack } from "expo-router";
+
+export default function RootLayout() {
+    return (
+        <Stack screenOptions={{}}>
+            <Stack.Screen name="index" options={{ title: "DEVember" }} />
+        </Stack>
+    );
+}
--- src/app/days/day1/index.tsx
+++ src/app/days/day1/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const DayDetailScreen = () => {
     return (
         <View>
+            <Stack.Screen options={{ title: "Day 1" }} />
+
             <Text>Day detail screen</Text>
         </View>
     );
