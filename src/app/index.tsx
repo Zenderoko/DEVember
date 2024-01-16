758aa5952d227a1f69696833276daaa51c63ac72
Alex <amonsalveru@gmail.com>
2024-01-16 00:27:19 -0300

    TS path aliases

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import {
-    StyleSheet,
-    Text,
-    View,
-    FlatList,
-    ActivityIndicator,
-} from "react-native";
-import DayListItem from "../../src/components/core/DayListItem";
+import { StyleSheet, View, FlatList } from "react-native";
+import DayListItem from "@components/core/DayListItem";
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
